6a856ca08df5d73dd2190528512cfa253c0022d8
Mathieu Leplatre <contact@mathieu-leplatre.info>
2012-03-21 17:32:11 +0100

    Improve setup

--- setup.py
+++ setup.py
@@ -8,18 +8,13 @@ here = os.path.abspath(os.path.dirname(__file__))
 setup(name       = 'landez',
     version      = '1.8.1',
     license      = 'LGPL',
-    description  = 'Build a MBTiles file from a tiles server or a Mapnik stylesheet.',
+    description  = 'Landez is a python toolbox to manipulate map tiles.',
     author       = "Mathieu Leplatre",
     author_email = "mathieu.leplatre@makina-corpus.com",
     url          = "https://github.com/makinacorpus/landez/",
     download_url = "http://pypi.python.org/pypi/landez/",
     long_description = open(os.path.join(here, 'README.rst')).read(),
     provides     = ['landez'],
-    entry_points = dict(
-        console_scripts = [
-        ]),
-    install_requires=[
-    ],
     packages     = find_packages(),
     platforms    = ['any'],
     keywords     = ['MBTiles', 'Mapnik'],
@@ -27,5 +22,5 @@ setup(name       = 'landez',
                     'Operating System :: OS Independent',
                     'Natural Language :: English',
                     'Topic :: Utilities',
-                    'Development Status :: 3 - Alpha'],
+                    'Development Status :: 5 - Production/Stable'],
 ) 
